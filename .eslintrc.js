6252cba2e7a82e98f915b64221aa4f477475522e
Dochkin Sergey <sergeyshark@gmail.com>
2020-07-16 11:36:12 +0300

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,53 @@
+module.exports = {
+	env: {
+		browser: true,
+		es6: true,
+	},
+	extends: [
+		'react-app',
+		'plugin:react/recommended',
+		'eslint:recommended',
+		'plugin:@typescript-eslint/eslint-recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:@typescript-eslint/recommended-requiring-type-checking',
+		'plugin:prettier/recommended',
+		'eslint-config-prettier',
+		'prettier',
+		'prettier/@typescript-eslint',
+	],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		project: './tsconfig.json',
+		ecmaVersion: 2018,
+		ecmaFeatures: {
+			jsx: true,
+		},
+		sourceType: 'module',
+	},
+	plugins: ['react', '@typescript-eslint', 'prettier'],
+	rules: {
+		'prettier/prettier': 'warn',
+		'require-await': 'error',
+		'@typescript-eslint/interface-name-prefix': [
+			'error',
+			{
+				prefixWithI: 'always',
+			},
+		],
+		'@typescript-eslint/prefer-regexp-exec': 'off',
+		'max-len': [
+			1,
+			{
+				code: 120,
+			},
+		],
+		camelcase: 'off',
+		'@typescript-eslint/camelcase': 'off',
+		// "react/jsx-max-props-per-line": [1],
+	},
+	settings: {
+		react: {
+			version: 'detect',
+		},
+	},
+};
---
+++ src/pages/Auth/index.js
@@ -0,0 +1,4 @@
+export {
+	Auth
+}
+from './Auth'
